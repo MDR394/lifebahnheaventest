43b2c7685a8b8c8c7f08ad162e6d652c9312ce0f
dawood <mdawoodrafique394@gmail.com>
2024-10-27 17:23:56 +0500

    updating the config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://lifebahnheaventestserver.vercel.app/"
+    ? "https://lifebahnheaventest.vercel.app/"
     : "http://localhost:3000";
 
 export default API_URL;
